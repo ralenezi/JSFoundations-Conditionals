7e754685dbe27a4555885eeee12576e40f37a726
Rahaf Alenezi <ralenezi71@gmail.com>
2020-10-19 17:35:08 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
 // Define the numbers and the operation here
-
+let firstNumber = 7
+let secondNumber = 6
+let op = '+'
 // Write if-statements here
+if (op == '+') {
+  console.log(`here is your addition operation: ${firstNumber + secondNumber}`)
+} else if (op == '-') {
+  console.log(`here is your addition operation: ${firstNumber - secondNumber}`)
+} else if (op == '*') {
+  console.log(`here is your addition operation: ${firstNumber * secondNumber}`)
+} else if (op == '/') {
+  console.log(`here is your addition operation: ${firstNumber / secondNumber}`)
+} else {
+  console.log(`insert a real operation plz`)
+}
